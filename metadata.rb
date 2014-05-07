cef825ad95b08a797e78183cf27c0903fba245ba
Roland Moriz <roland@moriz.de>
2014-05-07 22:03:44 +0200

    only compatible with centos…

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,12 @@ maintainer 'Sascha Moellering'
 maintainer_email 'sascha.moellering@gmail.com'
 license 'Apache v2.0'
 description 'Installs/Configures JBoss 7.3'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version '1.0.1'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '2.0.0'
 
 recipe 'jboss', 'installs jboss from the jboss community site'
 
-%w(debian ubuntu).each do |os|
+%w(centos).each do |os|
   supports os
 end
 
